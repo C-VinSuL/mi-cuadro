8258ab31d40563324520eebe2e2af9427e44ff31
Christopher Vincent Suarez Luna <vinsul1225@gmail.com>
2026-09-21 18:31:34 -0500

    SE CORRIGE PERMISOS DE USUARIO PARA QUE NO PUEDA APROBAR PRESTAMOS

--- src/config/permissions.js
+++ src/config/permissions.js
@@ -17,6 +17,7 @@ export const PERMISSIONS = {
     verPrestamos: true,
     solicitarPrestamo: true,
     aprobarPrestamos: true,
+  
     
     verHistorialCompleto: true,
     verConfiguracion: true
